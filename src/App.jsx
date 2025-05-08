b763386579e1c0f4fa6f75ae0610d7ab438f8af4
Eduardo-Muricy <Eduardo-Muricy@hotmail.com>
2025-05-07 21:42:44 -0300

    button change

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className='bg-[#6C757D] '>
-      <button className="bg-green-600 z-10 fixed backface-hidden flex justify-center items-center cursor-pointer w-15 h-15 rounded-full mt-185 ml-78 md:ml-455 md:mt-210 "> <a  target='_blank'
+      <button className="bg-green-600 z-10 fixed backface-hidden flex justify-center items-center cursor-pointer w-15 h-15 rounded-full mt-180 ml-78 md:ml-455 md:mt-210 "> <a  target='_blank'
                         href={`https://wa.me/5587988561908?text=Oi, vim pelo seu portifólio e gostaria de saber mais sobre o seu trabalho`}><WhatsappLogo className='text-white' size={40} /></a>
         
       </button>
